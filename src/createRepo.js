73bd52c88db0cc20af010fc4295070c7a6ad66a5
Brian Ephraim <brianephraim@gmail.com>
2017-05-31 19:01:43 -0400

    test

--- src/createRepo.js
+++ src/createRepo.js
@@ -10,15 +10,18 @@ inquirer.prompt([
   {
     type: 'input',
     name: 'phone',
-    message: 'What\'s your phone number',
-    validate: function (value) {
-      var pass = value.match(/^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
-      if (pass) {
-        return true;
-      }
+    message: 'NAME your package/repo',
+    // default: function () {
+    //   return 'Doe';
+    // },
+    // validate: function (value) {
+    //   var pass = value.match(/^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
+    //   if (pass) {
+    //     return true;
+    //   }
 
-      return 'Please enter a valid phone number';
-    }
+    //   return 'Please enter a valid phone number';
+    // }
   }
 ]).then(function (answers) {
     // Use user feedback for... whatever!! 
